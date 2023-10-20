df85bf400cffaa90967e4eb6d6576e5dde011f6f
Anand Lunawat <aplunawat@gmail.com>
2023-10-20 13:23:32 +0530

    Added experience in about tab

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,52 +1,66 @@
 'use client'
 
-import {PiGraduationCap} from "react-icons/pi"
+import { PiGraduationCap } from "react-icons/pi"
 import educationData from "../data/education"
 import experiences from "@/data/experiences";
-import {Chrono} from "react-chrono";
+import { Chrono } from "react-chrono";
 
 interface prop {
-  type : string
+  type: string
 }
 
-export default function Timeline (props : prop) {
+export default function Timeline(props: prop) {
 
-    return (
-      <Chrono
-      items={props.type==="experience" ? null : educationData}      
+  return (
+    <Chrono
+      items={props.type === "experience" ? null : educationData}
       mode="VERTICAL_ALTERNATING"
-      cardHeight={100} 
+      cardHeight={100}
       // itemWidth={150}
-      slideItemDuration={2000} 
+      slideItemDuration={2000}
       slideShow
       theme={{
         cardBgColor: "#E1E8EF",
         primary: "black",
-        secondary : "black",
-        titleColorActive : "#E1E8EF",                
-        cardTitleColor : "#0072b1",
-        titleColor : "#0072b1"
-      }}  
+        secondary: "black",
+        titleColorActive: "#E1E8EF",
+        cardTitleColor: "#0072b1",
+        titleColor: "#0072b1"
+      }}
       hideControls
       useReadMore
-      >        
-          {
-            props.type === "experience" ? <div className="flex flex-col gap-4">
-              <p className="text-[#0072b1] text-base font-semibold text-left">Mastork Technologies</p>
-              <p className="text-black text-sm font-semibold text-left">Fullstack Developer Intern</p>
-              <p className="text-sm font-normal">• Implemented a captivating React.js feature that showcased a dynamic component, resulting in a 40% increase in user engagement due to the elegant animations of food item names synchronized with their corresponding price fluctuations.</p>
-              <p className="text-sm font-normal">• Demonstrated proficiency in building interactive modals and pages that enhance the overall User Experience and facilitateseamless navigation throughout the application.</p>
-              <div className="flex flex-row gap-4 text-sm font-normal"><div className="bg-white rounded-lg p-2 ">ReactJS</div><div className="bg-white rounded-lg p-2 ">Tailwind</div></div>
-            </div> : <div className="chrono-icons"><PiGraduationCap style={{background : "white",color : "black",fontSize : "50px"}}/><PiGraduationCap style={{background : "white",color : "black",fontSize : "50px"}}/><PiGraduationCap style={{background : "white",color : "black",fontSize : "50px"}}/></div>    
-          }        
-      </Chrono>
-    )
+    >
+      {props.type === "experience" ? experiences.map((experience, index) => {
+        return (
+          <div key={index} className="VerticalItemWrapper-sc-1467v1d-1 uhss left vertical-item-row visible">
+            <div className="flex flex-col gap-4 ">
+              <p className="text-[#0072b1] text-base font-semibold text-left">{experience.companyName}</p>
+              <p className="text-black text-sm font-semibold text-left">{experience.role}</p>
+              <p className="text-sm font-normal">{experience.para1}</p>
+              <p className="text-sm font-normal">{experience.para2}</p>
+              <div className="flex flex-row gap-4 text-sm font-normal">
+                {experience.techStacks.map((techStack, index) => {
+                  return (
+                    <div key={index} className="bg-white rounded-lg p-2 ">{techStack}</div>
+                  )
+                })}
+              </div>
+            </div>
+            {/* <div className="absolute -right-[50%] TimelineTitleWrapper-sc-1427v1d-3 bTBgMV left">
+              <div className="TitleWrapper-sc-13izrht-0 eMDZDE timeline-item-title active rc-title">{experience.title}</div>
+            </div> */}
+          </div>
+        )
+      }) : <div className="chrono-icons"><PiGraduationCap style={{ background: "white", color: "black", fontSize: "50px" }} /><PiGraduationCap style={{ background: "white", color: "black", fontSize: "50px" }} /><PiGraduationCap style={{ background: "white", color: "black", fontSize: "50px" }} /></div>}
+    </Chrono>
+  )
 }
 
 
 
 // {
-//   props.type === "experience" ? experiences.map((experience)=> {
+//   props.type === "experience" ?
+// experiences.map((experience)=> {
 //     <div className="flex flex-col gap-4">
 //     <p className="text-[#0072b1] text-base font-semibold text-left">{experience.companyName}</p>
 //     <p className="text-black text-sm font-semibold text-left">{experience.role}</p>
@@ -54,5 +68,7 @@ export default function Timeline (props : prop) {
 //     <p className="text-sm font-normal">{experience.para2}</p>
 //     <div className="flex flex-row gap-4 text-sm font-normal"><div className="bg-white rounded-lg p-2 ">ReactJS</div><div className="bg-white rounded-lg p-2 ">Tailwind</div></div>
 //   </div>
-//   }) : <div className="chrono-icons"><PiGraduationCap style={{background : "white",color : "black",fontSize : "50px"}}/><PiGraduationCap style={{background : "white",color : "black",fontSize : "50px"}}/><PiGraduationCap style={{background : "white",color : "black",fontSize : "50px"}}/></div>    
+//   })
+//: <div className="chrono-icons"><PiGraduationCap style={{background : "white",color : "black",fontSize : "50px"}}/><PiGraduationCap style={{background : "white",color : "black",fontSize : "50px"}}/><PiGraduationCap style={{background : "white",color : "black",fontSize : "50px"}}/></div>
 // } 
+
--- data/experiences.ts
+++ data/experiences.ts
@@ -4,21 +4,23 @@ const experiences = [
         "companyName": "Mastork Technologies",
         "role" : "Fullstack Developer Intern",
         "para1": "• Implemented a captivating React.js feature that showcased a dynamic component, resulting in a 40% increase in user engagement due to the elegant animations of food item names synchronized with their corresponding price fluctuations.",
-        "para2" : "• Demonstrated proficiency in building interactive modals and pages that enhance the overall User Experience and facilitateseamless navigation throughout the application."
-
+        "para2" : "• Demonstrated proficiency in building interactive modals and pages that enhance the overall User Experience and facilitateseamless navigation throughout the application.",
+        "techStacks" : ["ReactJS","Tailwind"]
     },
-    {
-        "title": "FEBRUARY 2020",
-        "cardTitle": "PVG's Maharashtra Vidyalaya, Pune",
-        "cardSubtitle" : "12th Standard",
-        "cardDetailedText": "Percentage: 77.85%",
+    {        
+        "title": "Feb 2023 - July 2023",
+        "companyName": "Cybernetic Software Pvt Ltd",
+        "role" : "Frontend Developer",
+        "para1": "•Collaborated on refining the User Interface experience offered to clients at Cybernetic Software Pvt Ltd.",
+        "para2" : "• Used ReactJS and ChakraUI to craft engaging frontend solutions.",
+        "techStacks" : ["ReactJS","ChakraUI"]
     },
-    {
-        "title": "MAY 2018",
-        "cardTitle": "The Bishops Co-Ed School Kalyaninagar, Pune",
-        "cardSubtitle" : "10th Standard, ICSE",
-        "cardDetailedText": "Percentage: 87.7%",
-    }
+    // {
+    //     "title": "MAY 2018",
+    //     "cardTitle": "The Bishops Co-Ed School Kalyaninagar, Pune",
+    //     "cardSubtitle" : "10th Standard, ICSE",
+    //     "cardDetailedText": "Percentage: 87.7%",
+    // }
 ]
 
 export default experiences
