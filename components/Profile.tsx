e8a7544f2cba86cd667ab452cbc228f2c2d2664f
Anand Lunawat <aplunawat@gmail.com>
2023-10-27 00:45:47 +0530

    Updated profile with floating button

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -21,6 +21,9 @@ export default function Profile() {
         <div className={`${raleway.className}fixed max-lg:fixed flex flex-col right-2 bottom-16 lg:h-[75vh] lg:w-[25%] 2xl:w-[30%]`}>
             {
                 floatingButton && <div className="flex gap-6 lg:hidden flex-col">
+                    <Link href={"https://docs.google.com/uc?export=download&id=17gITpRlSwimM7RQzmxL81dXckCBzEsuS"} className="flex self-center justify-self-center mt-4 items-center rounded-full gap-4">
+                        <AiOutlineDownload />
+                    </Link>
                     <Link href="https://www.linkedin.com/in/anand-lunawat-aa220622b/" className="p-3">
                         <FaLinkedinIn style={{ color: "#0072b1" }} />
                     </Link>
@@ -37,7 +40,7 @@ export default function Profile() {
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="15" height="15" viewBox="0 0 48 48">
                             <path fill="#4caf50" d="M45,16.2l-5,2.75l-5,4.75L35,40h7c1.657,0,3-1.343,3-3V16.2z"></path><path fill="#1e88e5" d="M3,16.2l3.614,1.71L13,23.7V40H6c-1.657,0-3-1.343-3-3V16.2z"></path><polygon fill="#e53935" points="35,11.2 24,19.45 13,11.2 12,17 13,23.7 24,31.95 35,23.7 36,17"></polygon><path fill="#c62828" d="M3,12.298V16.2l10,7.5V11.2L9.876,8.859C9.132,8.301,8.228,8,7.298,8h0C4.924,8,3,9.924,3,12.298z"></path><path fill="#fbc02d" d="M45,12.298V16.2l-10,7.5V11.2l3.124-2.341C38.868,8.301,39.772,8,40.702,8h0 C43.076,8,45,9.924,45,12.298z"></path>
                         </svg>
-                    </Link>
+                    </Link>                    
                 </div>
             }
             <button onClick={() => { setFloatingButton(!floatingButton) }} className="rounded-full lg:hidden fixed right-2 bottom-2 w-fit mt-auto bg-white p-2 border-2">
