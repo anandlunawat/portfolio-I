b76dbd29db264ce76b56ff532d3b8a75075244c9
Anand Lunawat <aplunawat@gmail.com>
2023-10-26 16:55:05 +0530

    Updated Resume and Projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,16 @@
 import ProjectCards from "@/components/ProjectCards";
+import { Poppins } from "next/font/google"
+
+const poppins = Poppins({
+    weight: "500",
+    display: "swap",
+    preload: false
+});
 
 export default function Projects() {
     return (
-        <div className="bg-white w-full overflow-y-auto h-[75vh] 2xl:w-[70%] rounded-lg p-3">            
+        <div className="bg-white w-full flex flex-col gap-3 overflow-y-auto h-[75vh] 2xl:w-[70%] rounded-lg p-3">            
+            <span className={`text-[40px] text-center font-medium ${poppins.className}`}>Projects</span>
             <ProjectCards />                
         </div>
     )
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,16 @@
+import { Poppins } from "next/font/google"
+
+const poppins = Poppins({
+    weight: "500",
+    display: "swap",
+    preload: false
+});
+
 export default function Resume() {
     return (
-        <div className="w-full bg-white rounded-lg">            
-            <iframe title="Anand Lunawat | Resume" className="w-full rounded-lg h-[75vh] overflow-y-auto bg-white" src="https://drive.google.com/file/d/17gITpRlSwimM7RQzmxL81dXckCBzEsuS/preview"></iframe>
+        <div className="w-full flex flex-col gap-3 bg-white rounded-lg h-[75vh]">            
+            <span className={`text-[40px] text-center font-medium ${poppins.className}`}>Resume</span>
+            <iframe title="Anand Lunawat | Resume" className="w-full rounded-lg overflow-y-auto h-full bg-white" src="https://drive.google.com/file/d/17gITpRlSwimM7RQzmxL81dXckCBzEsuS/preview"></iframe>
         </div>
     )
 }
