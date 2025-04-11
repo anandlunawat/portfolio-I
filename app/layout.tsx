2890989686b9eaacb42c27963e425effb3811286
Anand Lunawat <aplunawat@gmail.com>
2025-04-12 02:06:56 +0530

    Height Ui fixes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
           <Navbar />
           <div className='lg:flex max-md:h-full gap-4 lg:flex-row w-full'>
             <Profile />
-            <div className='h-[80vh] overflow-y-scroll'>
+            <div className="max-h-screen lg:max-h-[80vh] overflow-y-auto">
               {children}
             </div>
           </div>
